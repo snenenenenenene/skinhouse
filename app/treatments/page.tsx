daf479cd5658383fd92e112cdef54c3153ff80f8
senne <sennebels@gmail.com>
2024-02-11 15:20:55 +0100

    fix: :bug: fix?

--- app/treatments/page.tsx
+++ app/treatments/page.tsx
@@ -1,4 +1,4 @@
 /* eslint-disable no-undef */
 export default function page({ children }: { children: React.ReactNode }) {
-  return <div className="flex flex-col">{children}</div>;
+  return <div>{children}</div>;
 }
